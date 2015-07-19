332fab65ff091bef2c5ddbdb1960484df77bbe13
Edwin Chen <edwinchen85@gmail.com>
2015-07-19 18:53:23 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$.getJSON('data.json', function(data) {
+	var output = '<ul>';
+	$.each(data, function(key, val) {
+		output += '<li>' + val.name + '</li>';
+	});
+	output +='</ul>';
+	$('#update').prepend(output);
+});
